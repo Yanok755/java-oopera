46a8b88ffcb611a266360b75e75e322f4a4cee06
Yanok755 <161292231+Yanok755@users.noreply.github.com>
2025-10-23 23:17:31 +0300

    Update Ballet.java

--- src/Ballet.java
+++ src/Ballet.java
@@ -1,14 +1,14 @@
 class Ballet extends MusicalShow {
-    private String choreographer;
+    private Person choreographer;
 
     public Ballet(String title, int duration, Director director,
-                  String musicAuthor, String librettoText,
-                  String choreographer) {
+                  Person musicAuthor, String librettoText,
+                  Person choreographer) {
         super(title, duration, director, musicAuthor, librettoText);
         this.choreographer = choreographer;
     }
 
-    public String getChoreographer() {
+    public Person getChoreographer() {
         return choreographer;
     }
 }
