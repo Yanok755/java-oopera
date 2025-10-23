c8335f3a99d8d2acde19bc23b51815bf0c52142c
Yanok755 <161292231+Yanok755@users.noreply.github.com>
2025-10-23 23:17:01 +0300

    Update Opera.java

--- src/Opera.java
+++ src/Opera.java
@@ -2,7 +2,7 @@ class Opera extends MusicalShow {
     private int choirSize;
 
     public Opera(String title, int duration, Director director,
-                 String musicAuthor, String librettoText,
+                 Person musicAuthor, String librettoText,
                  int choirSize) {
         super(title, duration, director, musicAuthor, librettoText);
         this.choirSize = choirSize;
